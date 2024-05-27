5322caccc49e315f0543e44be91060df194a2d3f
Jonathan Tramond <jonathan.tramond@gmail.com>
2024-05-27 11:57:19 +0200

    ajout des fichiers du projet

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+function toggleContent(mySection) {
+    var content = document.querySelector('.'+mySection+' div');
+    var icon = document.querySelector('.'+mySection+' button .fa-chevron-down');
+
+    if (content.classList.contains('collapsed')) {
+      content.classList.remove('collapsed');
+    } else {
+      content.classList.add('collapsed');
+    }
+
+    if (!icon.classList.contains('rotate') && !icon.classList.contains('rotate-back')) {
+      icon.classList.add('rotate');
+    } else if (icon.classList.contains('rotate')) {
+      icon.classList.remove('rotate');
+      icon.classList.add('rotate-back');
+    } else if (icon.classList.contains('rotate-back')) {
+      icon.classList.remove('rotate-back');
+      icon.classList.add('rotate');
+    }
+  }
